580ff2b1585bcdb162ad4b5878e0aa0d726aa761
Param-Harrison <paramanantham@live.com>
2019-04-14 21:40:51 +0300

    feat: basic react app using hooks and books api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [books, setBooks] = useState({items: []});
+  const onInputChange = (e) => {
+    setSearchTerm(e.target.value);
   }
+
+  let API_URL = `https://www.googleapis.com/books/v1/volumes`;
+
+  const fetchBooks = async () => {
+    const result = await axios.get(`${API_URL}?q=${searchTerm}`);
+    setBooks(result.data);
+  }
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    fetchBooks();
+  }
+
+  const bookAuthors = (authors) => {
+    if (authors.length <= 2) {
+      authors = authors.join(' and ')
+    }
+    else if (authors.length > 2) {
+      let lastAuthor = ' and ' + authors.slice(-1);
+      authors.pop();
+      authors = authors.join(', ');
+      authors += lastAuthor;
+    };
+    return authors;
+  }
+
+  return (
+    <section>
+      <form onSubmit={onSubmitHandler}>
+        <label>
+          <span>Search for books</span>
+          <input 
+            type="search" 
+            placeholder="microservice, restful design, etc.," 
+            value={searchTerm}
+            onChange={onInputChange}
+          />
+          <button type="submit">Search</button>
+        </label>
+      </form>
+      <ul>
+        {
+          books.items.map((book, index) => {
+            return (
+              <li key={index}>
+                <div>
+                  <img alt={`${book.volumeInfo.title} book`} src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} />
+                  <div>
+                    <h3>{ book.volumeInfo.title }</h3>
+                    <p>{ bookAuthors(book.volumeInfo.authors) }</p>
+                    <p>{book.volumeInfo.publishedDate}</p>
+                  </div>
+                </div>
+                <hr />
+              </li>
+            );
+          })
+        }
+      </ul>
+    </section>
+  );
 }
 
 export default App;
